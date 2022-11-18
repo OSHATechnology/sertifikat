16ce3c52214e250871fbb3644b0462c0fc0d0f5d
OSHATechnology <oshatechnology@gmail.com>
2022-11-18 15:58:26 +0700

    firts commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/page1', function () {
+    return view('sertifikat.page1');
+});
+Route::get('/page3', function () {
+    return view('sertifikat.page3');
+});
+// Route::get('/sertifikat', [Controller::class, 'cetak']);

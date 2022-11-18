3357b5ac0b42b6db5de1df17d45bbd6ad29dc760
OSHATechnology <oshatechnology@gmail.com>
2022-11-18 16:42:01 +0700

    layouts css

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,8 +7,7 @@
         <title>@yield('title')</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="assets/css/page1.css">
-        <link rel="stylesheet" href="assets/css/page3.css">
+        <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
         <div class="base-container">

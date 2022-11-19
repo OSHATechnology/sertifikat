a1227fc75b8a893e5710990bd74aa9bc7d4d269d
rizky <rizkyaditya2106@gmail.com>
2022-11-19 12:27:51 +0700

    add style-1.css

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,20 +8,21 @@
     <title>@yield('title')</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style-1.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Serif+Georgian:wght@400;700&display=swap">
 </head>
+
 <body>
     <div class="base-container">
         <div class="main-container">
             @yield('content')
         </div>
     </div>
-</div>
+    </div>
 </body>
 
 </html>
--- resources/views/sertifikat/page3.blade.php
+++ resources/views/sertifikat/page3.blade.php
@@ -1,136 +1,137 @@
 @extends('layouts.index')
-@section('title','Sertifikat Magenta BUMN Page 1')
+@section('title', 'Sertifikat Magenta BUMN Page 1')
 @section('content')
 
-<div class="container">
-    <section class="logo-container">
-        <div class="d-flex gap-5 justify-content-center">
-            <h3 class="judul">Daftar Nilai Magang Generasi Bertalenta</h3>
-            <div><img src="assets/images/magenta1.png" alt="magenta1" class="logo"></div>
-            <div><img src="assets/images/magenta2.png" alt="magenta2" class="logo"></div>
-            <div><img src="assets/images/magenta3.png" alt="magenta3" class="logo"></div>
-            <div><img src="assets/images/magenta4.png" alt="magenta4" class="logo"></div>
-            <div><img src="assets/images/magenta5.png" alt="magenta5" class="logo"></div>
-        </div>
-        <div class="d-flex gap-5 justify-content-left">
-            <h3 class="judul">PT Telkom Indonesia (Persero) Tbk</h3>
-        </div>
-    </section>
-    <section class="profile mt-3 mb-5">
-        <div class="d-flex flex-column align-items-left">
-            <table>
-                <tr>
-                    <td>Nama Mahasiswa    : Bagus Adi Gautama</td>
-                    <td>Fakultas/Jurusan  : Informatika/S1 Informatika</td>
-                </tr>
-                <tr>
-                    <td>NIM               : 1234567890</td>
-                    <td>Perguruan Tinggi  : Universitas Telkom</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>Posisi Magang     : UI Designer</td>
-                </tr>
-            </table>
-        </div>
-    </section>
-    <section class="content-container">
-        <div class="d-flex flex-column align-items-center">
-            <table>
-                <tr>
-                    <th rowspan="2">No</th>
-                    <th rowspan="2">Komponen</th>
-                    <th colspan="2">Daftar Isi</th>
-                </tr>
-                <tr>
-                    <th>Angka</th>
-                    <th>Huruf</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Integritas (Etika, Moral, dan Kesungguhan)</td>
-                    <td>89</td>
-                    <td><i>Delapan puluh sembilan</i></td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Ketepatan waktu dalam bekerja</td>
-                    <td>80</td>
-                    <td><i>Delapan puluh</i></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Keahlian berdasarkan bidang ilmu</td>
-                    <td>90</td>
-                    <td><i>Sembilan puluh</i></td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Kerjasama dalam tim</td>
-                    <td>88</td>
-                    <td><i>Delapan puluh delapan</i></td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>Komunikasi</td>
-                    <td>86</td>
-                    <td><i>Delapan puluh enam</i></td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>Penggunaan teknologi informasi</td>
-                    <td>87</td>
-                    <td><i>Delapan puluh tujuh</i></td>
-                </tr>
-                <tr>
-                    <td>7</td>
-                    <td>Pengembangan diri</td>
-                    <td>90</td>
-                    <td><i>Sembilan puluh</i></td>
-                </tr>
-                <tr>
-                    <th colspan="2">Total Nilai Pengembangan (A)</th>
-                    <td>610</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th colspan="2">Banyak Komponen (B)</th>
-                    <td>7</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th colspan="2">Rata - Rata Nilai (Q=A/B)</th>
-                    <td>87</td>
-                    <td></td>
-                </tr>
-            </table>
-        </div>
-    </section>
-    <section class="footer-container">
-        <div class="row">
-            <div class="d-flex justify-content-between">
-                <div class="d-flex flex-column align-items-center">
-                    <p>Kriteria Total Nilai Pembimbing Perusahaan</p>
-                    <table>
-                        <tr>
-                            <td>86-100  : Sangat Memuaskan (A)</td>
-                            <td>71-85   : Memuaskan (B)</td>
-                            <td><=70    : Cukup Memuaskan (C)</td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="d-flex flex-column align-items-center">
-                    <p class="from">Jakarta, 10 November 2022</p>
-                    <p class="who">Pembimbing</p>
-                    <p class="role">Andrian</p>
-                </div>
-                <div class="d-flex flex-column align-items-center code">
-                    <p>Sertifikat ini dapat diverifikasi pada QR Code berikut</p>
-                    <img src="/assets/images/qr-code.png" alt="qr-code">
+    <div class="container">
+        <section class="logo-container">
+            <div class="d-flex gap-5 justify-content-center">
+                <h3 class="judul">Daftar Nilai Magang Generasi Bertalenta</h3>
+                <div><img src="assets/images/magenta1.png" alt="magenta1" class="logo"></div>
+                <div><img src="assets/images/magenta2.png" alt="magenta2" class="logo"></div>
+                <div><img src="assets/images/magenta3.png" alt="magenta3" class="logo"></div>
+                <div><img src="assets/images/magenta4.png" alt="magenta4" class="logo"></div>
+                <div><img src="assets/images/magenta5.png" alt="magenta5" class="logo"></div>
+            </div>
+            <div class="d-flex gap-5 justify-content-left">
+                <h3 class="judul">PT Telkom Indonesia (Persero) Tbk</h3>
+            </div>
+        </section>
+        <section class="profile mt-3 mb-5">
+            <div class="d-flex flex-column align-items-left">
+                <table>
+                    <tr>
+                        <td>Nama Mahasiswa : Bagus Adi Gautama</td>
+                        <td>Fakultas/Jurusan : Informatika/S1 Informatika</td>
+                    </tr>
+                    <tr>
+                        <td>NIM : 1234567890</td>
+                        <td>Perguruan Tinggi : Universitas Telkom</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Posisi Magang : UI Designer</td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+        <section class="content-container">
+            <div class="d-flex flex-column align-items-center">
+                <table>
+                    <tr>
+                        <th rowspan="2">No</th>
+                        <th rowspan="2">Komponen</th>
+                        <th colspan="2">Daftar Isi</th>
+                    </tr>
+                    <tr>
+                        <th>Angka</th>
+                        <th>Huruf</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Integritas (Etika, Moral, dan Kesungguhan)</td>
+                        <td>89</td>
+                        <td><i>Delapan puluh sembilan</i></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Ketepatan waktu dalam bekerja</td>
+                        <td>80</td>
+                        <td><i>Delapan puluh</i></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Keahlian berdasarkan bidang ilmu</td>
+                        <td>90</td>
+                        <td><i>Sembilan puluh</i></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Kerjasama dalam tim</td>
+                        <td>88</td>
+                        <td><i>Delapan puluh delapan</i></td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Komunikasi</td>
+                        <td>86</td>
+                        <td><i>Delapan puluh enam</i></td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Penggunaan teknologi informasi</td>
+                        <td>87</td>
+                        <td><i>Delapan puluh tujuh</i></td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Pengembangan diri</td>
+                        <td>90</td>
+                        <td><i>Sembilan puluh</i></td>
+                    </tr>
+                    <tr>
+                        <th colspan="2">Total Nilai Pengembangan (A)</th>
+                        <td>610</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th colspan="2">Banyak Komponen (B)</th>
+                        <td>7</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th colspan="2">Rata - Rata Nilai (Q=A/B)</th>
+                        <td>87</td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+        <section class="footer-container">
+            <div class="row">
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-column align-items-center">
+                        <p>Kriteria Total Nilai Pembimbing Perusahaan</p>
+                        <table>
+                            <tr>
+                                <td>86-100 : Sangat Memuaskan (A)</td>
+                                <td>71-85 : Memuaskan (B)</td>
+                                <td>
+                                    <=70 : Cukup Memuaskan (C)</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="d-flex flex-column align-items-center">
+                        <p class="from">Jakarta, 10 November 2022</p>
+                        <p class="who">Pembimbing</p>
+                        <p class="role">Andrian</p>
+                    </div>
+                    <div class="d-flex align-items-center code gap-3">
+                        <p class="qr-code-text-1">Sertifikat ini dapat diverifikasi pada QR Code berikut</p>
+                        <img src="/assets/images/qr-code.png" alt="qr-code" class="qr-code">
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
-                
+        </section>
+    </div>
+
 @endsection
